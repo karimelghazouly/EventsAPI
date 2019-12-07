0217224c6d591b8200e98a6fc1a987d93aad7fed
Karim ElGhazouly <karimelghazouly@gmail.com>
2019-12-07 23:53:40 +0200

    Fix index.js errors
    Add body parser,create express object and use the eventRoutes routers

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const eventRoutes = require('./routes/events');
+const bodyParser = require('body-parser')
+
+let app = express();
 
 app.use(cors());
-
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   }),
 );
-
-
+app.use(bodyParser.json())
+app.use('/',eventRoutes);
 
 const PORT = process.env.PORT || 5000;
 
