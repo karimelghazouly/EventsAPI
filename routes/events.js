b49d23dedf0ea80df30a83c2cff3f1f6b96b14f9
Karim ElGhazouly <karimelghazouly@gmail.com>
2019-12-08 01:10:14 +0200

    Use validator module

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const EventController = require('../controllers/EventController');
+const EventValidator = require('../Validator/EventValidator');
 
-router.route('/').post(EventController.addEvent);
+router.route('/').post(EventValidator.validate(),EventController.addEvent);
 router.route('/').get(EventController.getAllEvents);
 
 module.exports = router;
