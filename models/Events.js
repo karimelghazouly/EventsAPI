283bdc1924aad24ed9b5b4bbc7fa8b17540d3cbd
Karim ElGhazouly <karimelghazouly@gmail.com>
2019-12-08 01:09:02 +0200

    use GeoJSON to save the coordinates

--- models/Events.js
+++ models/Events.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-mongoose.connect('mongodb://UserName:Password@ds131137.mlab.com:31137/brickbase-test', { useNewUrlParser: true });
+mongoose.connect('mongodb://elghazouly:KK123456@ds131137.mlab.com:31137/brickbase-test', { useNewUrlParser: true });
+
+const pointSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['Point'],
+    required: true
+  },
+  coordinates: {
+    type: [Number], // longitude first and then latitude
+    required: true
+  }
+});
+
+
 const EventSchema = new Schema(
   {
     end: { type: Date, required: true },
@@ -10,9 +24,9 @@ const EventSchema = new Schema(
     details: { type: String, required: true },
     location: {
       address: { type: String, required: true },
-      latLng: {
-        lat: { type: Number, required: true },
-        lng: { type: Number, required: true },
+      lngLat: {
+        type: pointSchema,
+        required: true
       },
     },
   },
