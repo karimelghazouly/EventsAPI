c07e31ec571607ef525c84050e0cb9ff4eb34271
Karim ElGhazouly <karimelghazouly@gmail.com>
2019-12-08 10:43:38 +0200

    remove validator/check which is deprecated

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const EventController = require('../controllers/EventController');
-const EventValidator = require('../Validator/EventValidator');
+const EventValidator = require('../validator/EventValidator');
 
 router.route('/').post(EventValidator.validate(),EventController.addEvent);
 router.route('/').get(EventController.getAllEvents);
