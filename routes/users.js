b9cc930f408449c31a023f7b0379061887179cb5
Karim ElGhazouly <karimelghazouly@gmail.com>
2019-12-10 21:29:20 +0200

    change files names

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const UserController = require('../controllers/UserController');
-
-router.route('/register').post(UserController.addUser);
-router.route('/login').post(UserController.login);
-router.route('/findAllUsersAndTheirEvents').get(UserController.authToken,UserController.findAllUsersAndTheirEvents);
-
-module.exports = router;
-
