6a4ab0578173fd866a749f96ffe68cec153c63e1
Karim ElGhazouly <karimelghazouly@gmail.com>
2019-12-11 15:30:31 +0200

    refactor code to follow convention

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -2,92 +2,98 @@ const Event = require('../models/Events');
 const User = require('../models/User');
 const ErrorChecker = require('../validator/ErrorChecker');
 
-module.exports = {
-  addEvent: async (req, res) => {
-    if (ErrorChecker.checkErrors(req, res))
-      return;
 
-    var user = req.user;
-    var data = req.body;
-    var offest = new Date().getTimezoneOffset() * 60 * 1000;
-    data.start = new Date(data.start) - new Date(data.start).getMinutes() * 1000 - offest
-    data.end = new Date(data.end) - new Date(data.end).getMinutes() * 1000 - offest;
+async function addEvent (req, res) {
+  if (ErrorChecker.checkErrors(req, res))
+    return;
 
-    const existing_event = await Event.findOne({
-      $and: [
-        {
-          'location.lngLat.coordinates': data.location.lngLat.coordinates
-        },
-        {
-          'start': data.start ,
-          'end': data.end
-        },
-        {
-          "ownerId": user._id
-        }
-      ],
+  var user = req.user;
+  var data = req.body;
+  var offest = new Date().getTimezoneOffset() * 60 * 1000;
+  data.start = new Date(data.start) - new Date(data.start).getMinutes() * 1000 - offest
+  data.end = new Date(data.end) - new Date(data.end).getMinutes() * 1000 - offest;
+
+  const existing_event = await Event.findOne({
+    $and: [
+      {
+        'location.lngLat.coordinates': data.location.lngLat.coordinates
+      },
+      {
+        'start': data.start ,
+        'end': data.end
+      },
+      {
+        "ownerId": user._id
+      }
+    ],
+  });
+
+  if (existing_event) {
+    res.status(500).json({
+      success: false,
+      message: 'An Event already exist at this venue on this day',
     });
+  } else {
 
-    if (existing_event) {
+    data.ownerId = user._id;
+    Event.create(data)
+      .then((event) => {
+        User.findOneAndUpdate(
+          user._id,
+          { $push: { events: event._id } },
+          (err,done) =>{
+            if(err)
+              console.log(err);
+          }
+        );
+
+        res.status(200).json({
+          success: true,
+          event,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          success: false,
+          message: 'An Error Occured, please try again later',
+        });
+      });
+  }
+}
+
+async function getAllEvents (req, res) {
+  await Event.find({})
+    .then((events) => {
+      res.json({ success: true, events });
+    })
+    .catch((err) => {
+      console.log(err);
       res.status(500).json({
         success: false,
-        message: 'An Event already exist at this venue on this day',
+        message: 'An Error Occured, please try again later',
       });
-    } else {
+    });
+}
 
-      data.ownerId = user._id;
-      Event.create(data)
-        .then((event) => {
-          User.findOneAndUpdate(
-            user._id,
-            { $push: { events: event._id } },
-            (err,done) =>{
-              if(err)
-                console.log(err);
-            }
-          );
+async function getAllEventsForUser (req, res) {
 
-          res.status(200).json({
-            success: true,
-            event,
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(500).json({
-            success: false,
-            message: 'An Error Occured, please try again later',
-          });
-        });
-    }
-  },
-  getAllEvents: async (req, res) => {
-    await Event.find({})
-      .then((events) => {
-        res.json({ success: true, events });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({
-          success: false,
-          message: 'An Error Occured, please try again later',
-        });
+  var user = req.user;
+  await Event.find({ ownerId: user._id })
+    .then((events) => {
+      res.json({ success: true, events });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        success: false,
+        message: 'An Error Occured, please try again later',
       });
-  },
+    });
+}
 
-  getAllEventsForUser: async (req, res) => {
-
-    var user = req.user;
-    await Event.find({ ownerId: user._id })
-      .then((events) => {
-        res.json({ success: true, events });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({
-          success: false,
-          message: 'An Error Occured, please try again later',
-        });
-      });
-  },
+module.exports = {
+  "addEvent":addEvent,
+  "getAllEvents":getAllEvents,
+  "getAllEventsForUser":getAllEventsForUser
 };
